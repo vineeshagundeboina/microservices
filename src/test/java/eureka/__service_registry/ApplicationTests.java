47376a0aa035d24971eb78e537fed121e2e8c07f
vineeshagundeboina <gundeboinavineesha123@gmail.com>
2024-12-22 19:16:50 +0530

    serviceregistry

---
+++ src/test/java/eureka/__service_registry/ApplicationTests.java
@@ -0,0 +1,13 @@
+package eureka.__service_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
